7d5c9e5ffae208fc103f0ff65fea18ba4ae0963f
Arthur Sfez <sfez_a@epitech.eu>
2012-12-05 15:51:55 +0000

    headers
    git-svn-id: svn+ssh://koala-scm.epitech.net/corewar-2016ed-2015s-2017si-liu_q@9 52f2329f-c769-44e9-9ead-ec7e55854066

---
+++ asm/include/cwstring.h
@@ -0,0 +1,23 @@
+/*
+** cwstring.h for corewar in /home/ignati_i//projects/corewar/corewar-2016ed-2015s-2017si-liu_q/include
+** 
+** Made by ivan ignatiev
+** Login   <ignati_i@epitech.net>
+** 
+** Started on  Tue Dec  4 17:04:55 2012 ivan ignatiev
+** Last update Wed Dec  5 16:48:12 2012 arthur sfez
+*/
+
+#ifndef CWLIB_H_
+# define CWLIB_H_
+
+# define BUFFER_SIZE 1024
+
+char	**my_split_string(char *str, char *separator,
+			  int quotes_mode);
+int	my_free_array(char **arr);
+char	*get_next_line(const int fd);
+int	my_strncpy_gnl(char *dest, char *src, int n);
+char	**my_add_to_array(char *str, char **arr);
+
+#endif /* CWLIB_H_ */
